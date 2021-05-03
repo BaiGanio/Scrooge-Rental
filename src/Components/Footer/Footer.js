98821f4089e7c364b76ad50edfe3413d3d902583
Lyuben Kikov <exceptionhelper@gmail.com>
2021-05-03 18:22:30 +0300

    Created project with initial setup.

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="text-center">
+      <h3>Developed by (BG) Team </h3>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store";
+import App from "./Components/App/App";
+// import { ServerCacheProvider } from "./server-cache/provider";
+// import { GlobalLoading } from "./containers/GlobalLoading/GlobalLoading";
+
+ReactDOM.render(
+  // <Provider store={store}>
+  //   <ServerCacheProvider>
+  //     <Router basename="/backoffice">
+  //       <App />
+  //       {/* <GlobalLoading /> */}
+  //     </Router>
+  //   </ServerCacheProvider>
+  // </Provider>,
+  <Router basename="/backoffice">
+    <App />
+         {/* <GlobalLoading /> */}
+  </Router>,
+  document.getElementById("root")
+);
