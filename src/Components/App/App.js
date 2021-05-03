cc86f68ca79de65666356df1c4af7f05e1227120
Lyuben Kikov <exceptionhelper@gmail.com>
2021-05-03 18:33:40 +0300

    Removed unused import

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,6 @@ import logo from './../../logo.svg';
 import './App.css';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import { Button, Paper } from '@material-ui/core';
 import Footer from '../Footer/Footer';
 
