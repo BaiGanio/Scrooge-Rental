69376d99a76eb861c56dd59bc9d41ea6a8655fb9
Lyuben Kikov <exceptionhelper@gmail.com>
2021-05-04 10:44:15 +0300

    Testing pipe once more

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,25 +8,24 @@ import Footer from '../Footer/Footer';
 
 function App() {
   return (
-    <div>
-      
-    <Container className="App">
-      <Paper>
-        <img src={logo} className="App-logo" alt="logo" />
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App + Material-UI
-        </Typography>
-        <Button variant="contained" color="primary">
-          Primary Button
-        </Button>
-        <Button variant="contained" color="secondary">
-          Secondary Button
-        </Button>
-      </Paper>
-      <div className="Footer">
-        <Footer />
-      </div>
-    </Container>
+    <div>      
+      <Container className="App">
+        <Paper>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Typography variant="h4" component="h1" gutterBottom>
+            Create React App + Material-UI
+          </Typography>
+          <Button variant="contained" color="primary">
+            Primary Button
+          </Button>
+          <Button variant="contained" color="secondary">
+            Secondary Button
+          </Button>
+        </Paper>
+        <div className="Footer">
+          <Footer />
+        </div>
+      </Container>
     </div>
   );
 }
