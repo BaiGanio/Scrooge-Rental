08b07aa07445be6cc03d22bed0d198d5be3df070
Lyuben Kikov <exceptionhelper@gmail.com>
2021-05-04 11:05:04 +0300

    Again

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="text-center">
-      <h3>Developed by Scrooge McDuck 1</h3>
+      <h3>Developed by Scrooge McDuck</h3>
     </div>
   );
 }
