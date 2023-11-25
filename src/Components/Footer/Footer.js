d2da5c9db63adf5b40aaaacc4cbaafe53dc8af3c
Lyuben Kikov <exceptionhelper@gmail.com>
2023-11-25 15:40:05 +0200

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <h3>Developed by Scrooge McDuck Global Corp.</h3>
+      <h3>Developed by Scrooge McDuck Global Corp. I'm kidding - developed by Lyuben Kikov</h3>
     </div>
   );
 }
